94410d2bd0985027df1c4e438c0061b909955195
Yvan Noah <yvannandy@gmail.com>
2025-11-04 21:03:40 +0300

    Fix docker problems

--- serverA/base.sql
+++ serverA/base.sql
@@ -1,9 +1,9 @@
-    CREATE DATABASE session;
+CREATE DATABASE session_db;
 
-    use session;
+USE session_db;
 
-    CREATE TABLE sessions (
-        id VARCHAR(128) PRIMARY KEY,
-        data TEXT,
-        last_access TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    );
+CREATE TABLE sessions (
+    id VARCHAR(128) PRIMARY KEY,
+    data TEXT,
+    last_access TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
